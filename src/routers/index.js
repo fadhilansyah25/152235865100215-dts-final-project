a6e152402f5fa0312b87208a489401412724d726
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2022-08-05 03:25:51 +0700

    fix: redirect file for netlify

--- src/routers/index.js
+++ src/routers/index.js
@@ -64,7 +64,7 @@ export default function Routers() {
           </ProtectLoginRegister>
         }
       />
-      <Route path="/*" element={<Homepage />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
